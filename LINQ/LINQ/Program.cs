26e81560bc42a1ccf22e693d8341f00997c89223
subramanian <subramanian.kumarappan@halliburton.com>
2023-12-10 22:49:32 +0530

    added entry point

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,5 +1,9 @@
-﻿int[] nums = { 20, 15, 31, 34, 35, 40, 50, 90, 99, 100 };
+﻿using LINQ;
+
+int[] nums = { 20, 15, 31, 34, 35, 40, 50, 90, 99, 100 };
 var gs = nums
  .ToLookup(k => k, k => nums.Where(n => n < k));
 
 var g = nums;
+
+LinqEntry.entryPoint();
